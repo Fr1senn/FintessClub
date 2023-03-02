446a5a347063b6fbcbc4159fd8f4411f8a04cb9b
Fr1senn <bombavlad1@gmail.com>
2023-03-02 17:04:23 +0200

    Added base URLs of the application and API.

--- FitnessClub/ClientApp/src/environments/environment.ts
+++ FitnessClub/ClientApp/src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  baseAppUrl: 'https://localhost:44453',
+  baseApiUrl: 'https://localhost:7123'
 };
 
 /*
