083e13ec0b6c57f87f32f0b818e1760efc2ba2ce
Fr1senn <bombavlad1@gmail.com>
2023-03-03 10:23:31 +0200

    Implemented method which returns subscriptions.

--- FitnessClub/ClientApp/src/app/services/subscription.service.ts
+++ FitnessClub/ClientApp/src/app/services/subscription.service.ts
@@ -1,13 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
+import { environment } from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class SubscriptionService {
   private readonly http: HttpClient;
+  private readonly apiUrl: string = environment.baseApiUrl;
 
   constructor(http: HttpClient) {
     this.http = http;
+  }
+
+  public getSubscriptions() {
+    return this.http.get(this.apiUrl + '/Subscription/GetSubscriptions');
   }
 }
