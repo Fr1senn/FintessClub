48e83f17cd2e895b66023f9e6977da454581f17a
Fr1senn <bombavlad1@gmail.com>
2023-03-03 10:15:46 +0200

    Subscription API controller has been created.

---
+++ FitnessClub/Controllers/SubscriptionController.cs
@@ -0,0 +1,13 @@
+﻿using FitnessClub.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FitnessClub.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SubscriptionController : ControllerBase
+    {
+    }
+}
