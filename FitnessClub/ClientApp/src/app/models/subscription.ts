e5b7e0c9e12ff1f02df458ee7ec07517b6aefa0f
Fr1senn <bombavlad1@gmail.com>
2023-03-03 10:10:56 +0200

    Subscription model has been updated.

--- FitnessClub/ClientApp/src/app/models/subscription.ts
+++ FitnessClub/ClientApp/src/app/models/subscription.ts
@@ -1,5 +1,15 @@
-﻿export interface Subscription {
+import { Discount } from "./discount"
+import { Order } from "./order"
+import { Review } from "./review"
+import { Wishlist } from "./wishlist"
+
+export interface Subscription {
   id: number,
   pricePerDay: number,
   title: NonNullable<string>
+
+  discounts: Array<Discount>,
+  orders: Array<Order>,
+  reviews: Array<Review>,
+  wishlists: Array<Wishlist>
 }
