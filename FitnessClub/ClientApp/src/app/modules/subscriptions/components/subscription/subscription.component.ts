9918c6d34eb3bd879266c8acb544edcda74e34ad
Fr1senn <bombavlad1@gmail.com>
2023-03-03 20:34:21 +0200

    Subscription component gets data from parent component now.

--- FitnessClub/ClientApp/src/app/modules/subscriptions/components/subscription/subscription.component.ts
+++ FitnessClub/ClientApp/src/app/modules/subscriptions/components/subscription/subscription.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Subscription } from "../../../../models/subscription";
 
 @Component({
   selector: 'app-subscription',
@@ -6,10 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./subscription.component.css']
 })
 export class SubscriptionComponent implements OnInit {
+  @Input() subscription: Subscription | undefined;
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
