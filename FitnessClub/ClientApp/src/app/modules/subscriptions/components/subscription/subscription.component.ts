60e17c53b8098ab97de6934ec065462e10905195
Fr1senn <bombavlad1@gmail.com>
2023-03-04 13:23:14 +0200

    Implemented method which calculates price with discount.

--- FitnessClub/ClientApp/src/app/modules/subscriptions/components/subscription/subscription.component.ts
+++ FitnessClub/ClientApp/src/app/modules/subscriptions/components/subscription/subscription.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Subscription } from "../../../../models/subscription";
+import { Discount } from "../../../../models/discount";
 
 @Component({
   selector: 'app-subscription',
@@ -12,5 +13,15 @@ export class SubscriptionComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public calculateDiscountPrice() {
+    let discount: Discount | undefined = this.subscription?.discounts.find(discount => discount.subscriptionId === this.subscription?.id);
+    if (discount !== undefined && this.subscription !== undefined) {
+      if (discount.isActive) {
+        return this.subscription.pricePerDay - (this.subscription.pricePerDay * discount.discountPercentage / 100);
+      }
+    }
+    return this.subscription?.pricePerDay;
   }
 }
