9f09c2ccc2a58b7cd2a4109a913779759f9d3357
Fr1senn <bombavlad1@gmail.com>
2023-03-08 12:47:06 +0200

    Created DTO models for the user.

---
+++ FitnessClub/Models/AuthModels.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FitnessClub.Models;
+
+public class LoginModel
+{
+    [EmailAddress] public string Email { get; set; } = String.Empty;
+
+    [MinLength(8)]
+    [RegularExpression("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*#?&_])[A-Za-z\\d@$!%*#?&_]{8,}$",
+        ErrorMessage =
+            "Password must contain at least one uppercase letter, one lowercase letter, one number and one special character")]
+    [DataType(DataType.Password)]
+    public string Password { get; set; } = String.Empty;
+}
+
+public class RegisterModel
+{
+    [Required]
+    [MinLength(2)]
+    [MaxLength(100)]
+    [RegularExpression("^[a-zA-Z]([a-zA-Z]| |-|')*$",
+        ErrorMessage = "The first name has to start with a letter and consist only of letters, space, - or '")]
+    public string FirstName { get; set; } = String.Empty;
+    [Required]
+    [MinLength(2)]
+    [MaxLength(100)]
+    [RegularExpression("^[a-zA-Z]([a-zA-Z]| |-|')*$",
+        ErrorMessage = "The last name has to start with a letter and consist only of letters, space, - or '")]
+    public string LastName { get; set; } = String.Empty;
+    [Required]
+    [MaxLength(320)]
+    [EmailAddress]
+    public string Email { get; set; } = String.Empty;
+    [Required]
+    [MinLength(8)]
+    [RegularExpression("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*#?&_])[A-Za-z\\d@$!%*#?&_]{8,}$",
+        ErrorMessage = "Password must contain at least one uppercase letter, one lowercase letter, one number and one special character")]
+    public string Password { get; set; } = String.Empty;
+    [Required]
+    public DateOnly Birthdate { get; set; }
+    public int Roleid { get; set; } = 1;
+}
