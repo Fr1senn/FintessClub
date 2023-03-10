ea6da3773f7b2f2fed939cbb8c8ac92d87288303
Fr1senn <bombavlad1@gmail.com>
2023-03-10 11:36:33 +0200

    Implemented logout method.

--- FitnessClub/ClientApp/src/app/components/navigation-bar/navigation-bar.component.ts
+++ FitnessClub/ClientApp/src/app/components/navigation-bar/navigation-bar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-navigation-bar',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navigation-bar.component.css']
 })
 export class NavigationBarComponent implements OnInit {
+  private readonly authService: AuthService;
+  private readonly router: Router;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate([""]);
+  }
 }
