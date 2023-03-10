42716d2a05ed947ce3a88ff19d26d00c6f2cbdf3
Fr1senn <bombavlad1@gmail.com>
2023-03-10 11:34:48 +0200

    Implemented interceptor which checks if the token has expired.

---
+++ FitnessClub/ClientApp/src/app/interceptors/jwt-expiration.interceptor.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { EMPTY, Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+import { JwtHelperService } from "@auth0/angular-jwt";
+
+@Injectable()
+export class JwtExpirationInterceptor implements HttpInterceptor {
+  private readonly authService: AuthService;
+  private readonly jwtHelperService: JwtHelperService;
+
+  constructor(authService: AuthService, jwtHelper: JwtHelperService) {
+    this.authService = authService;
+    this.jwtHelperService = jwtHelper;
+  }
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    if (this.authService.userToken && this.jwtHelperService.isTokenExpired(this.authService.userToken)) {
+      this.authService.logout();
+      return EMPTY;
+    }
+    return next.handle(request);
+  }
+}
