73539c997da56b0497aa268fc08b39caffac2629
Fr1senn <bombavlad1@gmail.com>
2023-03-14 08:55:23 +0200

    The UserProfile component has been injected.

--- FitnessClub/ClientApp/src/app/modules/profile/profile.module.ts
+++ FitnessClub/ClientApp/src/app/modules/profile/profile.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { UserProfileComponent } from "./components/user-profile/user-profile.component";
+import { HttpClientModule } from "@angular/common/http";
+import { RouterModule } from "@angular/router";
+import { NotAuthenticatedGuard } from "../../guards/not-authenticated.guard";
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    UserProfileComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path: 'Profile', component: UserProfileComponent, canActivate: [NotAuthenticatedGuard]}
+    ]),
+  ],
+  exports: [
+    UserProfileComponent
   ]
 })
-export class ProfileModule { }
+export class ProfileModule {
+}
