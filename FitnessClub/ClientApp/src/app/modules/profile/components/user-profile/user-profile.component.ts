38072ffbbd5ed51771161ea3f34ca9ecb30e5c14
Fr1senn <bombavlad1@gmail.com>
2023-03-18 11:30:18 +0200

    Implemented method that gets current user.

--- FitnessClub/ClientApp/src/app/modules/profile/components/user-profile/user-profile.component.ts
+++ FitnessClub/ClientApp/src/app/modules/profile/components/user-profile/user-profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user';
+import { UserService } from "../../../../services/user.service";
 
 @Component({
   selector: 'app-user-profile',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-profile.component.css']
 })
 export class UserProfileComponent implements OnInit {
+  public user: User | undefined;
+  private readonly userService: UserService;
 
-  constructor() { }
+  constructor(userService: UserService) {
+    this.userService = userService;
+  }
 
   ngOnInit(): void {
+    this.getCurrentUser();
+  }
+
+  public getCurrentUser(): void {
+    this.userService.getCurrentUser().subscribe((data: Object) => {
+      this.user = data as User;
+    });
   }
 
 }
