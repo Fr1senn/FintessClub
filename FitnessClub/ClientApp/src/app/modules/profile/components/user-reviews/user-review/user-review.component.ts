7a8934663f32f3712de5b116d916522c20366d2e
Fr1senn <bombavlad1@gmail.com>
2023-03-18 12:52:20 +0200

    The declared field stores a single review.

--- FitnessClub/ClientApp/src/app/modules/profile/components/user-reviews/user-review/user-review.component.ts
+++ FitnessClub/ClientApp/src/app/modules/profile/components/user-reviews/user-review/user-review.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Review } from "../../../../../models/review";
 
 @Component({
   selector: 'app-user-review',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-review.component.css']
 })
 export class UserReviewComponent implements OnInit {
-
+  @Input() review: Review | undefined;
   constructor() { }
 
   ngOnInit(): void {
