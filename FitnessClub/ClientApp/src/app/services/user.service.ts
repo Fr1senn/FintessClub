330352ee24740e5157bd6727c11acd438a57cf80
Fr1senn <bombavlad1@gmail.com>
2023-03-18 12:50:56 +0200

    Implemented a method that returns user reviews from the server.

--- FitnessClub/ClientApp/src/app/services/user.service.ts
+++ FitnessClub/ClientApp/src/app/services/user.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from "@angular/common/http";
 import { environment } from "../../environments/environment";
 import { Observable } from "rxjs";
 import { User } from "../models/user";
+import { Review } from "../models/review";
 
 @Injectable({
   providedIn: 'root'
@@ -16,5 +17,9 @@ export class UserService {
 
   public getCurrentUser(): Observable<User> {
     return this.http.get<User>(this.apiUrl + '/User/GetCurrentUser');
+  }
+
+  public getUserReviews(): Observable<Review[]> {
+    return this.http.get<Review[]>(this.apiUrl + '/User/GetUserReviews');
   }
 }
