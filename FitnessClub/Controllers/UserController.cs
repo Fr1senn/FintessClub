984456736fc07398e191aaac0a3692964f2f7b39
Fr1senn <bombavlad1@gmail.com>
2023-03-18 11:26:30 +0200

    The implemented method returns the current user.

--- FitnessClub/Controllers/UserController.cs
+++ FitnessClub/Controllers/UserController.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FitnessClub.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace FitnessClub.Controllers
 {
@@ -13,5 +15,22 @@ namespace FitnessClub.Controllers
     [Authorize]
     public class UserController : ControllerBase
     {
+        private readonly FitnessClubContext _context;
+        
+        public UserController(FitnessClubContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("GetCurrentUser")]
+        public async Task<IActionResult> GetCurrentUser()
+        {
+            int userId = Convert.ToInt32(User.FindFirst("Id")?.Value);
+            User? user = await _context.Users.FirstOrDefaultAsync(u => u.Id == userId);
+
+            if (user is null) return BadRequest("User not found");
+            
+            return Ok(user);
+        }
     }
 }
