3f9cfef7378441552598efb00c88b83bfd0f7538
Fr1senn <bombavlad1@gmail.com>
2023-03-18 13:37:25 +0200

    Declared fields and implemented methods which are necessary for the editing review.

--- FitnessClub/ClientApp/src/app/modules/profile/components/user-reviews/user-review/user-review.component.ts
+++ FitnessClub/ClientApp/src/app/modules/profile/components/user-reviews/user-review/user-review.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Review } from "../../../../../models/review";
+import { ReviewService } from "../../../../../services/review.service";
 
 @Component({
   selector: 'app-user-review',
@@ -7,10 +9,43 @@ import { Review } from "../../../../../models/review";
   styleUrls: ['./user-review.component.css']
 })
 export class UserReviewComponent implements OnInit {
-  @Input() review: Review | undefined;
-  constructor() { }
+  @Input() public review: Review | undefined;
+  public isEditing: boolean = false;
+  public form!: FormGroup;
+  public estimation: number = 1;
+  private readonly formBuilder: FormBuilder;
+  private readonly reviewService: ReviewService;
 
-  ngOnInit(): void {
+  constructor(formBuilder: FormBuilder, reviewService: ReviewService) {
+    this.formBuilder = formBuilder;
+    this.reviewService = reviewService;
   }
 
+  ngOnInit(): void {
+    this.form = this.getFormGroupInstance();
+    this.estimation = this.review?.estimation!;
+  }
+
+  public saveChanges() {
+    this.form.get('estimation')?.setValue(this.estimation);
+    this.reviewService.edit(this.form).subscribe(() => {
+      this.isEditing = false;
+      this.review!.estimation = this.estimation;
+      this.review!.reviewText = this.form.get('reviewText')?.value;
+    });
+  }
+
+  public changeEstimation(index: number) {
+    this.estimation = index + 1;
+  }
+
+  private getFormGroupInstance() {
+    return this.formBuilder.group({
+      id: new FormControl(this.review?.id),
+      userId: new FormControl(this.review?.userId),
+      subscriptionId: new FormControl(this.review?.subscriptionId),
+      reviewText: new FormControl(this.review?.reviewText, [Validators.required, Validators.maxLength(5000)]),
+      estimation: new FormControl(Math.round(this.review?.estimation!), [Validators.required])
+    });
+  }
 }
