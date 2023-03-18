2882d8c8b0d418084a1af86cf9841ec395ee670b
Fr1senn <bombavlad1@gmail.com>
2023-03-18 12:15:52 +0200

    UserReviews component has been injected.

--- FitnessClub/ClientApp/src/app/modules/profile/profile.module.ts
+++ FitnessClub/ClientApp/src/app/modules/profile/profile.module.ts
@@ -4,21 +4,29 @@ import { CredentialsComponent } from "./components/credentials/credentials.compo
 import { HttpClientModule } from "@angular/common/http";
 import { RouterModule } from "@angular/router";
 import { NotAuthenticatedGuard } from "../../guards/not-authenticated.guard";
+import { UserReviewsComponent } from "./components/user-reviews/user-reviews.component";
 
 
 @NgModule({
   declarations: [
-    CredentialsComponent
+    CredentialsComponent,
+    UserReviewsComponent
   ],
   imports: [
     CommonModule,
     HttpClientModule,
     RouterModule.forRoot([
-      {path: 'Profile', component: CredentialsComponent, canActivate: [NotAuthenticatedGuard]}
+      {
+        path: 'Profile', component: CredentialsComponent, canActivate: [NotAuthenticatedGuard],
+        children: [
+          {path: 'Reviews', component: UserReviewsComponent},
+        ]
+      }
     ]),
   ],
   exports: [
     CredentialsComponent,
+    UserReviewsComponent
   ]
 })
 export class ProfileModule {
