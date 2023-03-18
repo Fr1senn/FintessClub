1ef65ae8d64bcfe8c682150b32013940cc2fefd2
Fr1senn <bombavlad1@gmail.com>
2023-03-18 11:31:06 +0200

    Included all necessary data.

--- FitnessClub/ClientApp/src/app/models/user.ts
+++ FitnessClub/ClientApp/src/app/models/user.ts
@@ -1,10 +1,24 @@
-﻿export interface User {
+﻿import { Order } from "./order";
+import { Review } from "./review";
+import { TrainingSchedule } from "./trainingSchedule";
+import { Wishlist } from "./wishlist";
+import { Attendance } from "./attendance";
+import { Role } from "./role";
+
+export interface User {
   id: number,
   firstName: NonNullable<string>,
   lastName: NonNullable<string>,
-  email?: string,
+  email: NonNullable<string>,
   password: NonNullable<string>,
-  birthDate: Date,
+  birthDate: NonNullable<Date>,
   registrationDate: Date,
-  roleId?: number
+  roleId?: number,
+
+  attendances: Attendance[]
+  orders: Order[],
+  reviews: Review[],
+  role?: Role,
+  trainingSchedules: TrainingSchedule[],
+  wishlists: Wishlist[],
 }
