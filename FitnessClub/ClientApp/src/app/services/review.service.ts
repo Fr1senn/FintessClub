20934106f75119c2de6c00ea7be554c1893452ff
Fr1senn <bombavlad1@gmail.com>
2023-03-18 13:39:59 +0200

    Created service and implemented method that edits review.

---
+++ FitnessClub/ClientApp/src/app/services/review.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { environment } from "../../environments/environment";
+import { FormGroup } from '@angular/forms';
+import { Review } from "../models/review";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ReviewService {
+  private readonly http: HttpClient;
+  private readonly apiUrl: string = environment.baseApiUrl;
+
+  constructor(http: HttpClient) {
+    this.http = http;
+  }
+
+  public edit(form: FormGroup) {
+    return this.http.patch<Review>(this.apiUrl + '/Review/Edit', form.getRawValue(), {
+      observe: "response",
+      responseType: "text" as "json"
+    });
+  }
+}
