c8b99d7cd366f7824a8012f32121e4749c453299
Fr1senn <bombavlad1@gmail.com>
2023-03-30 14:54:58 +0300

    Implemented method that checks if subscription is already in user wishlist.

--- FitnessClub/ClientApp/src/app/modules/profile/components/wishlist/wishlist-item/wishlist-item.component.ts
+++ FitnessClub/ClientApp/src/app/modules/profile/components/wishlist/wishlist-item/wishlist-item.component.ts
@@ -70,4 +70,7 @@ export class WishlistItemComponent implements OnInit {
     });
   }
 
+  public isSubscriptionInWishlist(subscriptionId: number) {
+    return !!this.userWishlist?.find(item => item.subscriptionId === subscriptionId);
+  }
 }
