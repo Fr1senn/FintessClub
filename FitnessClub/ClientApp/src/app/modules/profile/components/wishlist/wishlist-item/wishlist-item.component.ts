642b740b5fa34ad4c94daa9f45e8a816f07514a6
Fr1senn <bombavlad1@gmail.com>
2023-03-30 14:58:57 +0300

    User now have the possibility to change subscription in his wishlist.

--- FitnessClub/ClientApp/src/app/modules/profile/components/wishlist/wishlist-item/wishlist-item.component.ts
+++ FitnessClub/ClientApp/src/app/modules/profile/components/wishlist/wishlist-item/wishlist-item.component.ts
@@ -3,6 +3,7 @@ import { Wishlist } from "../../../../../models/wishlist";
 import { UserService } from "../../../../../services/user.service";
 import { Discount } from "../../../../../models/discount";
 import { SubscriptionService } from "../../../../../services/subscription.service";
+import { Subscription } from "../../../../../models/subscription";
 
 @Component({
   selector: '[app-wishlist-item]',
@@ -14,6 +15,8 @@ export class WishlistItemComponent implements OnInit {
   @Output() public removeEvent: any = new EventEmitter<any>();
   public isEditing: boolean = false;
   public subscriptionDuration: number | undefined;
+  public subscriptionTitle: string | undefined;
+  public subscriptions: Subscription[] | undefined;
 
   private readonly userService: UserService;
   private readonly subscriptionService: SubscriptionService;
@@ -61,12 +64,14 @@ export class WishlistItemComponent implements OnInit {
 
   public updateWishlistItem(): void {
     let wishlistItem = {
-      subscriptionId: this.wishlistItem?.subscriptionId,
+      id: this.wishlistItem?.id,
+      subscriptionId: this.subscriptions?.find((subscription: Subscription) => subscription.title === this.subscriptionTitle)?.id!,
       subscriptionDuration: this.subscriptionDuration
     };
-    this.userService.updateWishlistItem(wishlistItem).subscribe((data: Wishlist) => {
+    this.userService.updateWishlistItem(wishlistItem).subscribe(() => {
       this.isEditing = false;
       this.wishlistItem!.daysAmount = this.subscriptionDuration!;
+      this.wishlistItem!.subscription!.title = this.subscriptionTitle!;
     });
   }
 
