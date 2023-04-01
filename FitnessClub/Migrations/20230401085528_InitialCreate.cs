67aea3b733f27530ed16e362643794f01f55847b
Fr1senn <bombavlad1@gmail.com>
2023-04-01 12:04:06 +0300

    Created migrations folder is created and the first migration is performed.

---
+++ FitnessClub/Migrations/20230401085528_InitialCreate.cs
@@ -0,0 +1,338 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace FitnessClub.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "exercises",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    title = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("exercises_pkey", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "roles",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    title = table.Column<string>(type: "character varying(64)", maxLength: 64, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("roles_pkey", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "subscriptions",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    price_per_day = table.Column<decimal>(type: "numeric(5,2)", precision: 5, scale: 2, nullable: false),
+                    title = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    rating = table.Column<decimal>(type: "numeric(4,2)", precision: 4, scale: 2, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("subscriptions_pkey", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    first_name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    last_name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    email = table.Column<string>(type: "character varying(320)", maxLength: 320, nullable: true),
+                    password = table.Column<string>(type: "character varying(64)", maxLength: 64, nullable: false),
+                    birth_date = table.Column<DateOnly>(type: "date", nullable: false),
+                    registration_date = table.Column<DateOnly>(type: "date", nullable: false, defaultValueSql: "CURRENT_DATE"),
+                    role_id = table.Column<int>(type: "integer", nullable: true, defaultValueSql: "1")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("users_pkey", x => x.id);
+                    table.ForeignKey(
+                        name: "users_role_id_fkey",
+                        column: x => x.role_id,
+                        principalTable: "roles",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "discounts",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    discount_percentage = table.Column<decimal>(type: "numeric(5,2)", precision: 5, scale: 2, nullable: false),
+                    is_active = table.Column<bool>(type: "boolean", nullable: false),
+                    subscription_id = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("discounts_pkey", x => x.id);
+                    table.ForeignKey(
+                        name: "discounts_subscriptioni_d_fkey",
+                        column: x => x.subscription_id,
+                        principalTable: "subscriptions",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "attendance",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    attendance_date = table.Column<DateOnly>(type: "date", nullable: false, defaultValueSql: "CURRENT_DATE"),
+                    user_id = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("attendance_pkey", x => x.id);
+                    table.ForeignKey(
+                        name: "attendance_user_id_fkey",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "orders",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    price = table.Column<decimal>(type: "numeric(7,2)", precision: 7, scale: 2, nullable: false),
+                    purchase_date = table.Column<DateOnly>(type: "date", nullable: false, defaultValueSql: "CURRENT_DATE"),
+                    user_id = table.Column<int>(type: "integer", nullable: true),
+                    subscription_id = table.Column<int>(type: "integer", nullable: true),
+                    days_amount = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("orders_pkey", x => x.id);
+                    table.ForeignKey(
+                        name: "orders_subscription_id_fkey",
+                        column: x => x.subscription_id,
+                        principalTable: "subscriptions",
+                        principalColumn: "id");
+                    table.ForeignKey(
+                        name: "orders_useri_d_fkey",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "reviews",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    review_text = table.Column<string>(type: "text", nullable: true),
+                    review_date = table.Column<DateOnly>(type: "date", nullable: false, defaultValueSql: "CURRENT_DATE"),
+                    user_id = table.Column<int>(type: "integer", nullable: true),
+                    subscription_id = table.Column<int>(type: "integer", nullable: true),
+                    estimation = table.Column<short>(type: "smallint", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("reviews_pkey", x => x.id);
+                    table.ForeignKey(
+                        name: "reviews_subscription_id_fkey",
+                        column: x => x.subscription_id,
+                        principalTable: "subscriptions",
+                        principalColumn: "id");
+                    table.ForeignKey(
+                        name: "reviews_user_id_fkey",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "training_schedules",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    training_date = table.Column<DateOnly>(type: "date", nullable: false),
+                    user_id = table.Column<int>(type: "integer", nullable: true),
+                    exercise_id = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("training_schedules_pkey", x => x.id);
+                    table.ForeignKey(
+                        name: "training_schedules_exercise_id_fkey",
+                        column: x => x.exercise_id,
+                        principalTable: "exercises",
+                        principalColumn: "id");
+                    table.ForeignKey(
+                        name: "training_schedules_user_id_fkey",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "wishlists",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    user_id = table.Column<int>(type: "integer", nullable: true),
+                    subscription_id = table.Column<int>(type: "integer", nullable: true),
+                    days_amount = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("wishlists_pkey", x => x.id);
+                    table.ForeignKey(
+                        name: "wishlists_subscription_id_fkey",
+                        column: x => x.subscription_id,
+                        principalTable: "subscriptions",
+                        principalColumn: "id");
+                    table.ForeignKey(
+                        name: "wishlists_user_id_fkey",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_attendance_user_id",
+                table: "attendance",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_discounts_subscription_id",
+                table: "discounts",
+                column: "subscription_id");
+
+            migrationBuilder.CreateIndex(
+                name: "exercises_title_key",
+                table: "exercises",
+                column: "title",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_orders_subscription_id",
+                table: "orders",
+                column: "subscription_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_orders_user_id",
+                table: "orders",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_reviews_subscription_id",
+                table: "reviews",
+                column: "subscription_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_reviews_user_id",
+                table: "reviews",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "roles_title_key",
+                table: "roles",
+                column: "title",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "subscriptions_title_key",
+                table: "subscriptions",
+                column: "title",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_training_schedules_exercise_id",
+                table: "training_schedules",
+                column: "exercise_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_training_schedules_user_id",
+                table: "training_schedules",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_users_role_id",
+                table: "users",
+                column: "role_id");
+
+            migrationBuilder.CreateIndex(
+                name: "users_email_key",
+                table: "users",
+                column: "email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_wishlists_user_id",
+                table: "wishlists",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "unique_user_subscription",
+                table: "wishlists",
+                columns: new[] { "subscription_id", "user_id" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "attendance");
+
+            migrationBuilder.DropTable(
+                name: "discounts");
+
+            migrationBuilder.DropTable(
+                name: "orders");
+
+            migrationBuilder.DropTable(
+                name: "reviews");
+
+            migrationBuilder.DropTable(
+                name: "training_schedules");
+
+            migrationBuilder.DropTable(
+                name: "wishlists");
+
+            migrationBuilder.DropTable(
+                name: "exercises");
+
+            migrationBuilder.DropTable(
+                name: "subscriptions");
+
+            migrationBuilder.DropTable(
+                name: "users");
+
+            migrationBuilder.DropTable(
+                name: "roles");
+        }
+    }
+}
