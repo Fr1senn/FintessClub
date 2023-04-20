7facf874b0ca0921c23db1ccf5699ad637226a8d
Fr1senn <bombavlad1@gmail.com>
2023-04-20 12:19:41 +0300

    Implemented DTO to transfer user credentials to the appropriate API controller.

--- FitnessClub/Models/AuthModels.cs
+++ FitnessClub/Models/AuthModels.cs
@@ -22,22 +22,43 @@ public class RegisterModel
     [RegularExpression("^[a-zA-Z]([a-zA-Z]| |-|')*$",
         ErrorMessage = "The first name has to start with a letter and consist only of letters, space, - or '")]
     public string FirstName { get; set; } = String.Empty;
+
     [Required]
     [MinLength(2)]
     [MaxLength(100)]
     [RegularExpression("^[a-zA-Z]([a-zA-Z]| |-|')*$",
         ErrorMessage = "The last name has to start with a letter and consist only of letters, space, - or '")]
     public string LastName { get; set; } = String.Empty;
+
     [Required]
     [MaxLength(320)]
     [EmailAddress]
     public string Email { get; set; } = String.Empty;
+
     [Required]
     [MinLength(8)]
     [RegularExpression("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*#?&_])[A-Za-z\\d@$!%*#?&_]{8,}$",
-        ErrorMessage = "Password must contain at least one uppercase letter, one lowercase letter, one number and one special character")]
+        ErrorMessage =
+            "Password must contain at least one uppercase letter, one lowercase letter, one number and one special character")]
     public string Password { get; set; } = String.Empty;
-    [Required]
-    public DateOnly Birthdate { get; set; }
+
+    [Required] public DateOnly Birthdate { get; set; }
     public int Roleid { get; set; } = 1;
+}
+
+public class UpdateCredentialsModel
+{
+    [Required]
+    [MaxLength(320)]
+    [EmailAddress]
+    public string Email { get; set; } = String.Empty;
+
+    [MinLength(8)]
+    [RegularExpression("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*#?&_])[A-Za-z\\d@$!%*#?&_]{8,}$",
+        ErrorMessage = "Password must contain at least one uppercase letter, one lowercase letter, one number and one special character")]
+    public string? Password { get; set; } = null;
+    
+    [Compare("Password",
+        ErrorMessage = "Passwords do not match")]
+    public string? PasswordConfirm { get; set; } = null;
 }
