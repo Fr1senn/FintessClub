f3e171b6f73f4af7da9680e115c268d4d23cc8f1
Fr1senn <bombavlad1@gmail.com>
2023-05-02 12:50:46 +0300

    Attendance controller has been created and implemented method that returns the entire attendance.

---
+++ FitnessClub/Controllers/AttendanceController.cs
@@ -0,0 +1,32 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using FitnessClub.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FitnessClub.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Roles = "admin")]
+    public class AttendanceController : ControllerBase
+    {
+        private readonly FitnessClubContext _context;
+
+        public AttendanceController(FitnessClubContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("GetAttendances")]
+        public async Task<IActionResult> GetAttendances()
+        {
+            var attendances = await _context.Attendances
+                .Include(a => a.User)
+                .AsNoTracking()
+                .ToListAsync();
+            return Ok(attendances);
+        }
+    }
+}
