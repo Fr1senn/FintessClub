09327b389be145eb4a71501f39c3c6becb3b9143
Fr1senn <bombavlad1@gmail.com>
2023-05-02 12:54:00 +0300

    AdminPanel component has been injected.

--- FitnessClub/ClientApp/src/app/modules/admin-panel/admin-panel.module.ts
+++ FitnessClub/ClientApp/src/app/modules/admin-panel/admin-panel.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AdminPanelComponent } from "./components/admin-panel/admin-panel.component";
+import { RouterModule } from "@angular/router";
+import { NotAuthenticatedGuard } from "../../guards/not-authenticated.guard";
 
 
 @NgModule({
   declarations: [
+    AdminPanelComponent
   ],
   imports: [
     CommonModule,
+    RouterModule.forRoot([
+      {
+        path: 'AdminPanel', component: AdminPanelComponent, canActivate: [NotAuthenticatedGuard]}
+    ])
   ],
   exports: [
+    AdminPanelComponent
   ]
 })
 export class AdminPanelModule {
