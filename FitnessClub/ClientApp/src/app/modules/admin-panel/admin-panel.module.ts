02263968b9e763a812fcd97f055a15641a6f4138
Fr1senn <bombavlad1@gmail.com>
2023-05-02 12:54:46 +0300

    Attendance component has been injected.

--- FitnessClub/ClientApp/src/app/modules/admin-panel/admin-panel.module.ts
+++ FitnessClub/ClientApp/src/app/modules/admin-panel/admin-panel.module.ts
@@ -2,22 +2,29 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminPanelComponent } from "./components/admin-panel/admin-panel.component";
 import { RouterModule } from "@angular/router";
+import { AttendanceComponent } from "./components/attendance/attendance.component";
 import { NotAuthenticatedGuard } from "../../guards/not-authenticated.guard";
 
 
 @NgModule({
   declarations: [
-    AdminPanelComponent
+    AdminPanelComponent,
+    AttendanceComponent
   ],
   imports: [
     CommonModule,
     RouterModule.forRoot([
       {
-        path: 'AdminPanel', component: AdminPanelComponent, canActivate: [NotAuthenticatedGuard]}
+        path: 'AdminPanel', component: AdminPanelComponent, canActivate: [NotAuthenticatedGuard],
+        children: [
+          {path: 'Attendance', component: AttendanceComponent},
+        ]
+      }
     ])
   ],
   exports: [
-    AdminPanelComponent
+    AdminPanelComponent,
+    AttendanceComponent
   ]
 })
 export class AdminPanelModule {
