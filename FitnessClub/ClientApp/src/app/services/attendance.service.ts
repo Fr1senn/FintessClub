c144c4a465cb0ca0858d87b06d2a86121c0beb48
Fr1senn <bombavlad1@gmail.com>
2023-05-03 11:49:29 +0300

    Created attendance service and implemented all necessary methods.

---
+++ FitnessClub/ClientApp/src/app/services/attendance.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { environment } from "../../environments/environment";
+import { Observable } from "rxjs";
+import { Attendance } from "../models/attendance";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AttendanceService {
+  private readonly http: HttpClient;
+  private readonly apiUrl: string = environment.baseApiUrl;
+
+  constructor(http: HttpClient) {
+    this.http = http;
+  }
+
+  public getAttendance() :Observable<Attendance>{
+    return this.http.get<Attendance>(this.apiUrl + '/Attendance/GetAttendance');
+  }
+
+  public getAttendanceByUser(userFirstName: string, userLastName: string): Observable<Attendance> {
+    return this.http.get<Attendance>(this.apiUrl + '/Attendance/GetAttandanceByUser', {
+      params: {
+        'userFirstName': userFirstName,
+        'userLastName': userLastName
+      }
+    });
+  }
+
+  public getAttendanceFromTill(attendanceDateFrom: string, attendanceDateTill: string) {
+    return this.http.get<Attendance>(this.apiUrl + '/Attendance/GetAttendanceFromTill', {
+      params: {
+        'attendanceDateFrom': attendanceDateFrom,
+        'attendanceDateTill': attendanceDateTill
+      }
+    });
+  }
+}
