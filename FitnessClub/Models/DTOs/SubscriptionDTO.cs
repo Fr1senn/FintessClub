830e11330d53267664299133fda0272fde0b19e5
Fr1senn <bombavlad1@gmail.com>
2023-05-04 13:17:31 +0300

    Implemented the DTO subscription model.

---
+++ FitnessClub/Models/DTOs/SubscriptionDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace FitnessClub.Models.DTOs
+{
+    public class SubscriptionDTO
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public double? PricePerDay { get; set; }
+        public byte? DiscountPercentage { get; set; }
+        public bool? IsActive { get; set; }
+    }
+}
