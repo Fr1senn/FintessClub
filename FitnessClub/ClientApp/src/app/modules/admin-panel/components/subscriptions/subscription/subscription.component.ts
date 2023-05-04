c10c4175e962a11f49f33b1c3647b0363f533cc6
Fr1senn <bombavlad1@gmail.com>
2023-05-04 13:22:15 +0300

    Implemented everything you need for the component to work correctly.

--- FitnessClub/ClientApp/src/app/modules/admin-panel/components/subscriptions/subscription/subscription.component.ts
+++ FitnessClub/ClientApp/src/app/modules/admin-panel/components/subscriptions/subscription/subscription.component.ts
@@ -1,15 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Discount } from '../../../../../models/discount';
+import { Subscription } from '../../../../../models/subscription';
+import { SubscriptionService } from '../../../../../services/subscription.service';
 
 @Component({
-  selector: 'app-subscription',
+  selector: '[app-subscription]',
   templateUrl: './subscription.component.html',
   styleUrls: ['./subscription.component.css']
 })
 export class SubscriptionComponent implements OnInit {
+  @Input() public subscription: Subscription | undefined;
+  public isEditing: boolean = false;
+  public editedSubscription = {
+    id: 0,
+    title: '',
+    pricePerDay: 0,
+    discountPercentage: 0,
+    isActive: false
+  };
 
-  constructor() { }
+  private readonly subscriptionService: SubscriptionService;
+
+  constructor(subscriptionService: SubscriptionService) {
+    this.subscriptionService = subscriptionService;
+  }
 
   ngOnInit(): void {
+    this.setDefaultSubscriptionValue();
+  }
+
+  public getSubscriptionDiscount() {
+    return this.subscription?.discounts.find((discount: Discount) => discount.subscriptionId === this.subscription?.id);
+  }
+
+  public updateSubscription() {
+    if (this.editedSubscription.isActive === undefined) {
+      this.editedSubscription.isActive = false;
+    }
+    this.subscriptionService.updateSubscription(this.editedSubscription).subscribe(() => {
+      this.subscription!.title = this.editedSubscription.title;
+      this.subscription!.pricePerDay = this.editedSubscription.pricePerDay;
+      this.subscription!.discounts.find(discount => discount.subscriptionId === this.subscription?.id)!.discountPercentage = this.editedSubscription.discountPercentage;
+      this.subscription!.discounts.find(discount => discount.subscriptionId === this.subscription?.id)!.isActive = this.editedSubscription.isActive;
+    })
+  }
+
+  private setDefaultSubscriptionValue() {
+    this.editedSubscription.id = this.subscription?.id!;
+    this.editedSubscription.title = this.subscription?.title!;
+    this.editedSubscription.pricePerDay = this.subscription?.pricePerDay!;
+    this.editedSubscription.discountPercentage = this.subscription?.discounts.find(discount => discount.subscriptionId === this.subscription?.id)?.discountPercentage!;
+    this.editedSubscription.isActive = this.subscription?.discounts.find(discount => discount.subscriptionId === this.subscription?.id)?.isActive!;
   }
 
 }
