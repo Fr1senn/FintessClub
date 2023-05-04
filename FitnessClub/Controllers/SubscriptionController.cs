bf619ee743fae4b854283e81c36abaa3233a6673
Fr1senn <bombavlad1@gmail.com>
2023-05-04 13:18:27 +0300

    Implemented a method that updates the subscription and, if necessary, the data on the subscription discount.

--- FitnessClub/Controllers/SubscriptionController.cs
+++ FitnessClub/Controllers/SubscriptionController.cs
@@ -1,4 +1,5 @@
 ﻿using FitnessClub.Models;
+using FitnessClub.Models.DTOs;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -48,6 +49,61 @@ namespace FitnessClub.Controllers
                 .AsNoTracking()
                 .Where(s => s.Title.ToLower().Contains(subscriptionTitle.ToLower()))
                 .ToListAsync());
+        }
+
+        [HttpPost("UpdateSubscription")]
+        public async Task<IActionResult> UpdateSubscription([FromBody] SubscriptionDTO subscriptionDTO)
+        {
+            Subscription? subscription = await _context.Subscriptions
+                .Include(s => s.Discounts)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(s => s.Id == subscriptionDTO.Id);
+
+            if (subscription is null) return NotFound();
+
+            if (subscriptionDTO.Title is not null)
+            {
+                subscription.Title = subscriptionDTO.Title;
+            }
+            if (subscriptionDTO.PricePerDay is not null)
+            {
+                subscription.PricePerDay = (decimal)subscriptionDTO.PricePerDay;
+            }
+            
+            if (subscription.Discounts.Count == 0)
+            {
+                if (subscriptionDTO.DiscountPercentage is not null && subscriptionDTO.IsActive is not null)
+                {
+                    _context.Discounts.Add(new Discount
+                    {
+                        SubscriptionId = subscriptionDTO.Id,
+                        DiscountPercentage = (decimal)subscriptionDTO.DiscountPercentage,
+                        IsActive = (bool)subscriptionDTO.IsActive
+                    });
+                }
+            }
+
+            if (subscription.Discounts is not null && subscription.Discounts.Any())
+            {
+                foreach (var discount in subscription.Discounts)
+                {
+                    if (subscriptionDTO.DiscountPercentage is not null)
+                    {
+                        discount.DiscountPercentage = (decimal)subscriptionDTO.DiscountPercentage;
+                    }
+                    if (subscriptionDTO.IsActive is not null)
+                    {
+                        discount.IsActive = (bool)subscriptionDTO.IsActive;
+                    }
+
+                    _context.Discounts.Update(discount);
+                }
+            }
+
+            _context.Subscriptions.Update(subscription);
+            await _context.SaveChangesAsync();
+
+            return Ok(subscription);
         }
     }
 }
