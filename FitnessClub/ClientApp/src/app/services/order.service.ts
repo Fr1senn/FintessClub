e83077fa26804eb026bc7894272a81896c5f11aa
Fr1senn <bombavlad1@gmail.com>
2023-05-08 16:02:52 +0300

    A methods that gives necessary data from the DB server were implemented.

--- FitnessClub/ClientApp/src/app/services/order.service.ts
+++ FitnessClub/ClientApp/src/app/services/order.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { Order } from '../models/order';
 
 @Injectable({
   providedIn: 'root'
@@ -15,6 +16,36 @@ export class OrderService {
   }
 
   public buySubscription(orderData: Object) {
-    return this.http.post(this.apiUrl + '/Order/BuySubscription', orderData, {responseType: "text" as "json"});
+    return this.http.post(this.apiUrl + '/Order/BuySubscription', orderData, { responseType: "text" as "json" });
+  }
+
+  public getOrders(): Observable<Order[]> {
+    return this.http.get<Order[]>(this.apiUrl + '/Order/GetOrders');
+  }
+
+  public getOrdersFromTill(ordersDateFrom: string, ordersDateTill: string): Observable<Order[]> {
+    return this.http.get<Order[]>(this.apiUrl + '/Order/GetOrdersFromTill', {
+      params: {
+        'ordersDateFrom': ordersDateFrom,
+        'ordersDateTill': ordersDateTill
+      }
+    });
+  }
+
+  public getOrdersByUser(userFirstName: string, userLastName: string): Observable<Order[]> {
+    return this.http.get<Order[]>(this.apiUrl + '/Order/GetOrdersByUser', {
+      params: {
+        'userFirstName': userFirstName,
+        'userLastName': userLastName
+      }
+    });
+  }
+
+  public deleteOrder(orderId: number) {
+    return this.http.delete(this.apiUrl + '/Order/DeleteOrder', {
+      params: {
+        'orderId': orderId,
+      }
+    });
   }
 }
