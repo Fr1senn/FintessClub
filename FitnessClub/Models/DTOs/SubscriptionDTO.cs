46b1913a346d61f18590743923fc40b6d70b153d
Fr1senn <bombavlad1@gmail.com>
2023-05-10 11:32:02 +0300

    Subscription DTO with possible discount was implemented.

--- FitnessClub/Models/DTOs/SubscriptionDTO.cs
+++ FitnessClub/Models/DTOs/SubscriptionDTO.cs
@@ -1,4 +1,6 @@
-﻿namespace FitnessClub.Models.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FitnessClub.Models.DTOs
 {
     public class SubscriptionDTO
     {
@@ -7,5 +9,17 @@
         public double? PricePerDay { get; set; }
         public byte? DiscountPercentage { get; set; }
         public bool? IsActive { get; set; }
+    }
+
+    public class SubcriptionWithNewDiscountDTO
+    {
+        [MinLength(3)]
+        public string Title { get; set; }
+        [Range(0.1, Double.MaxValue)]
+        public decimal PricePerDay { get; set; }
+        [Range(1,99, ErrorMessage ="You can\'t set discount less then 1 and greater then 99")]
+        public byte? DiscountPercentage { get; set; }
+        public bool? IsActive { get; set; }
+        public bool IsCreateDiscount { get; set; } = false;
     }
 }
