2c1a858afe412fe5328b7d11a57111815016c463
Fr1senn <bombavlad1@gmail.com>
2023-05-12 12:20:27 +0300

    Review DTO was implemented.

---
+++ FitnessClub/Models/DTOs/ReviewDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FitnessClub.Models.DTOs
+{
+    public class ReviewDTO
+    {
+        [MinLength(3)]
+        public string ReviewText { get; set; }
+        [Range(1, 5)]
+        public short Estimation { get; set; }
+        public int SubscriptionId { get;set; }
+    }
+}
