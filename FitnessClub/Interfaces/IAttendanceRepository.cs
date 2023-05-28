f997168d6a70faddbc0f949e3cf98c77d1c2d7cc
Fr1senn <bombavlad1@gmail.com>
2023-05-28 13:12:55 +0300

    Added contract for AttendanceRepository.

---
+++ FitnessClub/Interfaces/IAttendanceRepository.cs
@@ -0,0 +1,12 @@
+﻿using FitnessClub.Models;
+
+namespace FitnessClub.Interfaces
+{
+    public interface IAttendanceRepository
+    {
+        Task<IEnumerable<Attendance>> GetAttendances();
+        Task<IEnumerable<Attendance>> GetAttendancesByUser(string userFirstName, string userLastName);
+        Task<IEnumerable<Attendance>> GetAttendancesFromTill(DateOnly? attendanceDateFrom, DateOnly? attendanceDateTill);
+        Task AddAttendance(int userId);
+    }
+}
