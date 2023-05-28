33a2613cde621dfa147d28ae682a053b03de296c
Fr1senn <bombavlad1@gmail.com>
2023-05-28 13:18:56 +0300

    Methods of AttendanceController were changed.

--- FitnessClub/Controllers/AttendanceController.cs
+++ FitnessClub/Controllers/AttendanceController.cs
@@ -1,9 +1,7 @@
-﻿using System.IdentityModel.Tokens.Jwt;
+﻿using FitnessClub.Interfaces;
 using FitnessClub.Models;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace FitnessClub.Controllers
 {
@@ -12,68 +10,55 @@ namespace FitnessClub.Controllers
     [Authorize(Roles = "admin, manager")]
     public class AttendanceController : ControllerBase
     {
-        private readonly FitnessClubContext _context;
+        private readonly IAttendanceRepository _attendanceRepository;
 
-        public AttendanceController(FitnessClubContext context)
+        public AttendanceController(IAttendanceRepository attendanceRepository)
         {
-            _context = context;
+            _attendanceRepository = attendanceRepository;
         }
 
         [HttpGet("GetAttendance")]
         public async Task<IActionResult> GetAttendance()
         {
-            List<Attendance> attendance = await _context.Attendances
-                .Include(a => a.User)
-                .AsNoTracking()
-                .ToListAsync();
-            return Ok(attendance);
+            List<Attendance> attendances = (await _attendanceRepository.GetAttendances()).ToList();
+            return Ok(attendances);
         }
 
         [HttpGet("GetAttandanceByUser")]
         public async Task<IActionResult> GetAttandanceByUser([FromQuery] string userFirstName, [FromQuery] string userLastName)
         {
-            User? user = await _context.Users.FirstOrDefaultAsync(u => u.FirstName == userFirstName && u.LastName == userLastName);
-            if (user is null) return BadRequest();
-            List<Attendance> attendance = await _context.Attendances
-                .Include(a => a.User)
-                .Where(a => a.User.Id == user.Id)
-                .AsNoTracking()
-                .ToListAsync();
-            return Ok(attendance);
+            try
+            {
+                List<Attendance> attendances = (await _attendanceRepository.GetAttendancesByUser(userFirstName, userLastName)).ToList();
+                return Ok(attendances);
+            }
+            catch (NullReferenceException exception)
+            {
+                return BadRequest(exception.Message);
+            }
         }
 
         [HttpGet("GetAttendanceFromTill")]
         public async Task<IActionResult> GetAttendanceFromTill([FromQuery] DateOnly? attendanceDateFrom, [FromQuery] DateOnly? attendanceDateTill)
         {
-            attendanceDateFrom ??= DateOnly.MinValue;
-            attendanceDateTill ??= DateOnly.MaxValue;
+            List<Attendance> attendances = (await _attendanceRepository.GetAttendancesFromTill(attendanceDateFrom, attendanceDateTill)).ToList();
 
-            List<Attendance> attendance = await _context.Attendances
-                .Include(a => a.User)
-                .Where(a => a.AttendanceDate >= attendanceDateFrom && a.AttendanceDate <= attendanceDateTill)
-                .AsNoTracking()
-                .ToListAsync();
-
-            return Ok(attendance);
+            return Ok(attendances);
         }
 
         [HttpPost("CreateAttendance")]
         [Authorize(Roles = "admin")]
         public async Task<IActionResult> CreateAttendance([FromBody] int userId)
         {
-            User? user = await _context.Users.FindAsync(userId);
-
-            if (user is null) return BadRequest();
-
-            Attendance attendance = new Attendance()
+            try
             {
-                UserId = userId,
-            };
-
-            _context.Attendances.Add(attendance);
-            await _context.SaveChangesAsync();
-
-            return Ok();
+                await _attendanceRepository.AddAttendance(userId);
+                return Ok();
+            }
+            catch (NullReferenceException exception)
+            {
+                return BadRequest(exception.Message);
+            }
         }
     }
 }
