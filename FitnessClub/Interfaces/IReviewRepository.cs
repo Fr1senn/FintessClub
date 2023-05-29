e0d230fe5eae216aea768862205bfa8c3b69bc93
Fr1senn <bombavlad1@gmail.com>
2023-05-29 11:43:04 +0300

    Added contract for ReviewRepository.

---
+++ FitnessClub/Interfaces/IReviewRepository.cs
@@ -0,0 +1,12 @@
+﻿using FitnessClub.Models;
+using FitnessClub.Models.DTOs;
+
+namespace FitnessClub.Interfaces
+{
+    public interface IReviewRepository
+    {
+        Task EditReview(Review review, int userId);
+        Task DeleteReview(int reviewId);
+        Task CreateReview(ReviewDTO reviewDTO, int userId);
+    }
+}
