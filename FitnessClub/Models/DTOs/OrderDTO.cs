32500d9671efb13d34504560dd5a51e1d67551f0
Fr1senn <bombavlad1@gmail.com>
2023-05-29 11:11:21 +0300

    Added validation for the daysAmount field.

--- FitnessClub/Models/DTOs/OrderDTO.cs
+++ FitnessClub/Models/DTOs/OrderDTO.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,6 +10,7 @@ namespace FitnessClub.Models
     {
         public int UserId { get; set; }
         public int SubscriptionId { get; set; }
+        [Range(1, int.MaxValue, ErrorMessage = "You can't set the subscription duration to less than 1 day")]
         public int DaysAmount { get; set; }
     }
 }
