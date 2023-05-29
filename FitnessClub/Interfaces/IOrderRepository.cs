61ba26b6dfe5e3a2ff24329d4a562112cfd50232
Fr1senn <bombavlad1@gmail.com>
2023-05-29 11:09:19 +0300

    Added contract for OrderRepository.

---
+++ FitnessClub/Interfaces/IOrderRepository.cs
@@ -0,0 +1,13 @@
+﻿using FitnessClub.Models;
+
+namespace FitnessClub.Interfaces
+{
+    public interface IOrderRepository
+    {
+        Task AddSubscription(OrderDTO orderDTO);
+        Task<IEnumerable<Order>> GetOrders();
+        Task<IEnumerable<Order>> GetOrdersFromTill(DateOnly? ordersDateFrom, DateOnly? ordersDateTill);
+        Task<IEnumerable<Order>> GetOrdersByUser(string userFirstName, string userLastName);
+        Task DeleteOrder(int orderId);
+    }
+}
